f8e2e52534df91c38fc2474330d52b0f60e1ccfc
CoderQCFromScraatch <qcoll@QUINLAN-COMPUTER>
2022-05-08 16:08:13 -0700

    willowThingys

--- src/main/java/info/coderqc/mc/bettermc/item/AmberPickaxeItem.java
+++ src/main/java/info/coderqc/mc/bettermc/item/AmberPickaxeItem.java
@@ -1,7 +1,14 @@
 
 package info.coderqc.mc.bettermc.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+import info.coderqc.mc.bettermc.init.BettermcModItems;
 
 public class AmberPickaxeItem extends PickaxeItem {
 	public AmberPickaxeItem() {
@@ -29,11 +36,6 @@ public class AmberPickaxeItem extends PickaxeItem {
 			public Ingredient getRepairIngredient() {
 				return Ingredient.of(new ItemStack(BettermcModItems.AMBER.get()));
 			}
-		},
-
-				1, -3f,
-
-				new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+		}, 1, -3f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
 	}
-
 }
