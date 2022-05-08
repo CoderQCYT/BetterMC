3e00808d584199334e576c404142531341422252
githubpub900 <rylan@192.168.0.26>
2022-05-08 15:47:56 -0700

    more stuff

---
+++ src/main/java/info/coderqc/mc/bettermc/recipes/brewing/IdklolBrewingRecipe.java
@@ -0,0 +1,44 @@
+
+package info.coderqc.mc.bettermc.recipes.brewing;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import info.coderqc.mc.bettermc.init.BettermcModPotions;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class IdklolBrewingRecipe implements IBrewingRecipe {
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new IdklolBrewingRecipe()));
+	}
+
+	@Override
+	public boolean isInput(ItemStack input) {
+		Item inputItem = input.getItem();
+		return (inputItem == Items.POTION || inputItem == Items.SPLASH_POTION || inputItem == Items.LINGERING_POTION)
+				&& PotionUtils.getPotion(input) == Potions.WATER;
+	}
+
+	@Override
+	public boolean isIngredient(ItemStack ingredient) {
+		return ingredient.getItem() == Items.PHANTOM_MEMBRANE;
+	}
+
+	@Override
+	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+		if (isInput(input) && isIngredient(ingredient)) {
+			return PotionUtils.setPotion(new ItemStack(input.getItem()), BettermcModPotions.LEVITATION.get());
+		}
+		return ItemStack.EMPTY;
+	}
+}
