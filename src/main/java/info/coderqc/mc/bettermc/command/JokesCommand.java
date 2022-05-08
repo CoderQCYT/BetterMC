b84c25cb717bdf86320e73f5af25d4288e74d1d6
githubpub900 <rylan@192.168.0.26>
2022-05-08 14:39:06 -0700

    nothing, mcreator forced me to commit

--- src/main/java/info/coderqc/mc/bettermc/command/JokesCommand.java
+++ src/main/java/info/coderqc/mc/bettermc/command/JokesCommand.java
@@ -1,9 +1,28 @@
 
 package info.coderqc.mc.bettermc.command;
 
+import org.checkerframework.checker.units.qual.s;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.common.util.FakePlayerFactory;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.CommandSourceStack;
+
+import java.util.HashMap;
+import java.util.Arrays;
+
+import info.coderqc.mc.bettermc.procedures.JokesCommandExecutedProcedure;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
 @Mod.EventBusSubscriber
 public class JokesCommand {
-
 	@SubscribeEvent
 	public static void registerCommand(RegisterCommandsEvent event) {
 		event.getDispatcher()
@@ -14,15 +33,12 @@ public class JokesCommand {
 
 	private static int execute(CommandContext<CommandSourceStack> ctx) {
 		ServerLevel world = ctx.getSource().getLevel();
-
 		double x = ctx.getSource().getPosition().x();
 		double y = ctx.getSource().getPosition().y();
 		double z = ctx.getSource().getPosition().z();
-
 		Entity entity = ctx.getSource().getEntity();
 		if (entity == null)
 			entity = FakePlayerFactory.getMinecraft(world);
-
 		HashMap<String, String> cmdparams = new HashMap<>();
 		int[] index = {-1};
 		Arrays.stream(ctx.getInput().split("\\s+")).forEach(param -> {
@@ -31,9 +47,7 @@ public class JokesCommand {
 			index[0]++;
 		});
 
-		JokesCommandExecutedProcedure.execute();
-
+		JokesCommandExecutedProcedure.execute(world, entity);
 		return 0;
 	}
-
 }
