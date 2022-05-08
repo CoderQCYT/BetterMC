ee7036aa5a0b3813ec53cd4aa20887096da6e4e7
githubpub900 <rylan@192.168.0.26>
2022-05-08 14:27:30 -0700

    added coconuts

---
+++ src/main/java/info/coderqc/mc/bettermc/item/CoconutItem.java
@@ -0,0 +1,22 @@
+
+package info.coderqc.mc.bettermc.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
+
+public class CoconutItem extends Item {
+	public CoconutItem() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(64).rarity(Rarity.COMMON)
+				.food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f)
+
+						.build()));
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 32;
+	}
+}
