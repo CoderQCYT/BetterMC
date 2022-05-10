aaf3f6e7e7460db82b605002ef9c7f2f50a3c85b
CoderQCFromScraatch <qcoll@QUINLAN-COMPUTER>
2022-05-09 21:00:59 -0700

    vertical slabs fix

--- src/main/java/info/coderqc/mc/bettermc/block/VerticalDarkoakSlabBlock.java
+++ src/main/java/info/coderqc/mc/bettermc/block/VerticalDarkoakSlabBlock.java
@@ -4,15 +4,24 @@ package info.coderqc.mc.bettermc.block;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
@@ -23,9 +32,12 @@ import java.util.Collections;
 import info.coderqc.mc.bettermc.init.BettermcModBlocks;
 
 public class VerticalDarkoakSlabBlock extends Block {
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
 	public VerticalDarkoakSlabBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.WOOD).strength(1f, 10f).noOcclusion()
 				.isRedstoneConductor((bs, br, bp) -> false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
@@ -36,6 +48,41 @@ public class VerticalDarkoakSlabBlock extends Block {
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 0;
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		Vec3 offset = state.getOffset(world, pos);
+		switch ((Direction) state.getValue(FACING)) {
+			case SOUTH :
+			default :
+				return box(0, 0, 0, 16, 16, 8).move(offset.x, offset.y, offset.z);
+			case NORTH :
+				return box(0, 0, 8, 16, 16, 16).move(offset.x, offset.y, offset.z);
+			case EAST :
+				return box(0, 0, 0, 8, 16, 16).move(offset.x, offset.y, offset.z);
+			case WEST :
+				return box(8, 0, 0, 16, 16, 16).move(offset.x, offset.y, offset.z);
+		}
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		;
+		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
 	}
 
 	@Override
