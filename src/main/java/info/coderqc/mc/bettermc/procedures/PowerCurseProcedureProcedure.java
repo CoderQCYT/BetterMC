9e983e3b4e5fb776654a77191f67becb197b005f
CoderQCFromScraatch <qcoll@QUINLAN-COMPUTER>
2022-05-09 17:06:44 -0700

    ay yo

--- src/main/java/info/coderqc/mc/bettermc/procedures/PowerCurseProcedureProcedure.java
+++ src/main/java/info/coderqc/mc/bettermc/procedures/PowerCurseProcedureProcedure.java
@@ -1,8 +1,28 @@
 package info.coderqc.mc.bettermc.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.damagesource.DamageSource;
 
 import javax.annotation.Nullable;
+
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Comparator;
+
+import info.coderqc.mc.bettermc.init.BettermcModEnchantments;
 
 @Mod.EventBusSubscriber
 public class PowerCurseProcedureProcedure {
@@ -10,15 +30,16 @@ public class PowerCurseProcedureProcedure {
 	public static void onEntityAttacked(LivingAttackEvent event) {
 		if (event != null && event.getEntity() != null) {
 			execute(event, event.getEntity().level, event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity(),
-					event.getSource().getEntity());
+					event.getSource().getEntity(), event.getAmount());
 		}
 	}
 
-	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
-		execute(null, world, x, y, z, entity, sourceentity);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity, double amount) {
+		execute(null, world, x, y, z, entity, sourceentity, amount);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity,
+			double amount) {
 		if (entity == null || sourceentity == null)
 			return;
 		if (EnchantmentHelper.getItemEnchantmentLevel(BettermcModEnchantments.POWER_CURSE.get(),
@@ -39,6 +60,13 @@ public class PowerCurseProcedureProcedure {
 						_entity.addEffect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 60, 1));
 				}
 			}
+		}
+		if (EnchantmentHelper.getItemEnchantmentLevel(BettermcModEnchantments.VOID_STRIKE.get(),
+				(sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0) {
+			if (entity instanceof LivingEntity _entity)
+				_entity.hurt(new DamageSource("custom").bypassArmor(),
+						(float) (amount * (EnchantmentHelper.getItemEnchantmentLevel(BettermcModEnchantments.VOID_STRIKE.get(),
+								(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) + 1)));
 		}
 	}
 }
