dab8d37820f2df9b298c098aa1d64f366c7ec342
qcoll <qcoll@QUINLAN-COMPUTER>
2022-05-09 17:14:39 -0700

    MCreator Merge commit 'a0d89b5ba0229933646316d83019a0c693866872' into 4d67bb1cd981fd0d99fb3e59833998f6ca551cf4

--- src/main/java/info/coderqc/mc/bettermc/init/BettermcModEnchantments.java
+++ src/main/java/info/coderqc/mc/bettermc/init/BettermcModEnchantments.java
@@ -10,10 +10,12 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.item.enchantment.Enchantment;
 
+import info.coderqc.mc.bettermc.enchantment.VoidStrikeEnchantment;
 import info.coderqc.mc.bettermc.enchantment.PowerCurseEnchantment;
 import info.coderqc.mc.bettermc.BettermcMod;
 
 public class BettermcModEnchantments {
 	public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, BettermcMod.MODID);
 	public static final RegistryObject<Enchantment> POWER_CURSE = REGISTRY.register("power_curse", () -> new PowerCurseEnchantment());
+	public static final RegistryObject<Enchantment> VOID_STRIKE = REGISTRY.register("void_strike", () -> new VoidStrikeEnchantment());
 }
